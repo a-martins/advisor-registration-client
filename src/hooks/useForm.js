a3e5da230156fdc0eb25a809a3ad55a1f1dc51f1
Alexandre Martins <alexandremartins.ti@gmail.com>
2020-10-25 23:25:14 -0300

    Added validate errors handle and currentId reset

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,19 +1,34 @@
 import { useState } from 'react'
 
-const useForm = (initialFieldValues) => {
+const useForm = (initialFieldValues, validate, setCurrentId) => {
     const [values, setValues] = useState(initialFieldValues);
+    const [errors, setErrors] = useState([]);
 
     const handleInputChange = e => {
-        const { name, value } = e.target;
+        const { name, value } = e.target
+        const fieldValue = { [name]: value }
+
         setValues({
             ...values,
-            [name]: value
+            ...fieldValue
         })
+        validate(fieldValue)
+    }
+
+    const resetForm = () => {
+        setValues({
+            ...initialFieldValues
+        })
+        setErrors({})
+        setCurrentId(0)
     }
 
     return {
         values,
         setValues,
+        errors,
+        setErrors,
+        resetForm,
         handleInputChange
     }
 }
