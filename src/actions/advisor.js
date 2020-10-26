fd2c3035b337911d1aff60dde6f7a155a6404526
Alexandre Martins <alexandremartins.ti@gmail.com>
2020-10-25 23:23:43 -0300

    Added actions create, update and delete

--- src/actions/advisor.js
+++ src/actions/advisor.js
@@ -7,6 +7,11 @@ export const ACTION_TYPE = {
     FETCH_ALL: 'FETCH_ALL'
 }
 
+const formateData = data => ({
+    ...data,
+    registryNumber: parseInt(data.registryNumber ? data.registryNumber : 0)
+})
+
 export const fetchAll = () => dispatch => {
     api.advisor()
         .fetchAll()
@@ -15,6 +20,50 @@ export const fetchAll = () => dispatch => {
                 type: ACTION_TYPE.FETCH_ALL,
                 payload: response.data
             })
+        })
+        .catch(err => console.log(err))
+}
+
+export const create = (data, onSuccess) => dispatch => {
+    data = formateData(data);
+    api.advisor()
+        .create(data)
+        .then(response => {
+            dispatch({
+                type: ACTION_TYPE.CREATE,
+                payload: response.data
+            })
+            onSuccess()
+        })
+        .catch(err => console.log(err))
+}
+
+export const update = (id, data, onSuccess) => dispatch => {
+    data = formateData(data);
+    api.advisor()
+        .update(id, data)
+        .then(response => {
+            dispatch({
+                type: ACTION_TYPE.UPDATE,
+                payload: {
+                    id,
+                    ...data
+                }
+            })
+            onSuccess()
+        })
+        .catch(err => console.log(err))
+}
+
+export const Delete = (id, onSuccess) => dispatch => {
+    api.advisor()
+        .delete(id)
+        .then(response => {
+            dispatch({
+                type: ACTION_TYPE.DELETE,
+                payload: id
+            })
+            onSuccess()
         })
         .catch(err => console.log(err))
 }
