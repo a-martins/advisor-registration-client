1697500c298d315a9b34ceb73e79defc148b1f9f
Alexandre Martins <alexandremartins.ti@gmail.com>
2020-10-25 23:28:19 -0300

    Added store provider, toast provider and materialize-ui container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { store } from './actions/store'
+import { Provider } from 'react-redux'
+
+import Advisors from './components/Advisors'
+import { Container } from '@material-ui/core'
+import { ToastProvider } from 'react-toast-notifications'
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Provider store={store}>
+      <ToastProvider autoDismiss={true}>
+        <Container maxWidth="lg">
+          <Advisors />
+        </Container>
+      </ToastProvider>
+    </Provider>
   );
 }
 
